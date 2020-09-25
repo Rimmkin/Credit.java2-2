5ab336524bd86bff3a49c136246350765d76d09c
Rimmkin <70262551+Rimmkin@users.noreply.github.com>
2020-09-25 09:43:37 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,10 @@ public class Main {
         double p=0.0999;
         int paymentOneYear = (int) service.calculate(s, t, p);
         System.out.println(paymentOneYear);
-        s = 1_000_000;
         t = 24;
-        p = 0.0999;
         int paymentTwoYears = (int) service.calculate(s, t, p);
         System.out.println(paymentTwoYears);
-        s = 1000000;
         t = 36;
-        p=0.0999;
         int paymentThreeYears = (int) service.calculate(s, t, p);
         System.out.println(paymentThreeYears);
     }
